2fedfef0fd47a5fbede9830917da4e1625dde4f1
Nelson Lee <leelhn2345@outlook.com>
2023-12-20 18:44:32 +0800

    chore: change autocomplete opts

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -22,7 +22,7 @@ opt.colorcolumn = "80"
 opt.signcolumn = "yes"
 opt.cmdheight = 1
 opt.scrolloff = 10
-opt.completeopt = "menuone,noinsert,noselect"
+opt.completeopt = "menuone,noinsert,noselect,preview"
 
 -- Behaviour
 opt.hidden = true
