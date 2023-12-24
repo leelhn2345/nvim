037869fcdf7f9ac7b4e0b87647ad71e257abb6bb
Nelson Lee <leelhn2345@outlook.com>
2023-12-24 17:27:50 +0800

    chore: remove additional regex highlight

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,40 +1,43 @@
 local config = function()
-  require("nvim-treesitter.configs").setup({
-    -- indent = {
-    --   enable = true,
-    -- },
-    autotag = {
-      enable = true,
-    },
-    ensure_installed = {
-      "rust",
-      "markdown",
-      "markdown_inline",
-      "json",
-      "javascript",
-      "typescript",
-      "yaml",
-      "html",
-      "css",
-      "scss",
-      "bash",
-      "lua",
-      "dockerfile",
-      "python",
-      "toml",
-      "regex",
-    },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = true,
-    },
-  })
+	require("nvim-treesitter.configs").setup({
+		-- indent = {
+		--   enable = true,
+		-- },
+		autotag = {
+			enable = true,
+		},
+		ensure_installed = {
+			"rust",
+			"markdown",
+			"markdown_inline",
+			"json",
+			"javascript",
+			"typescript",
+			"yaml",
+			"html",
+			"css",
+			"scss",
+			"bash",
+			"lua",
+			"dockerfile",
+			"python",
+			"toml",
+			"regex",
+			"vim",
+			"vimdoc",
+			"query",
+			"c",
+		},
+		sync_install = false,
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+	})
 end
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = config,
+	"nvim-treesitter/nvim-treesitter",
+	config = config,
 }
-
