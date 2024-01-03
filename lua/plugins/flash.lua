deefa71997a0de46f4d3f7cba0fdc7ed74528f7d
Nelson Lee <leelhn2345@outlook.com>
2024-01-03 16:03:13 +0800

    chore: change keymaps for flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,39 +1,23 @@
 return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
-	opts = {
-		modes = {
-			char = {
-				enabled = false,
-				multi_line = false,
-			},
+	opts = {},
+	keys = {
+		{
+			"s",
+			mode = { "n" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
 		},
 	},
-	dependencies = { "folke/which-key.nvim" },
-	config = function()
-		local wk = require("which-key")
-		wk.register({
-			["<leader>j"] = {
-				name = "Flash",
-				u = {
-					function()
-						require("flash").jump()
-					end,
-					"Jump",
-				},
-				t = {
-					function()
-						require("flash").treesitter()
-					end,
-					"Treesitter",
-				},
-				j = {
-					function()
-						require("flash").treesitter_search()
-					end,
-					"Treesitter Search",
-				},
-			},
-		})
-	end,
 }
