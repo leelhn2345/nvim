72323596d9a4c93ee911f5b4f3736d97764d9573
Nelson Lee <leelhn2345@outlook.com>
2024-01-11 16:55:08 +0800

    feat: "trouble close" on close for auto-sessions

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,11 +1,11 @@
 return {
-	"rmagatti/auto-session",
-	-- can't be VeryLazy
-	-- Mason can't be VeryLazy as well due to LSP on 1st file read
-	config = function()
-		require("auto-session").setup({
-			auto_session_allowed_dirs = { "~/code/*", "~/.config/nvim" },
-			pre_save_cmds = { "Neotree close" },
-		})
-	end,
+  "rmagatti/auto-session",
+  -- can't be VeryLazy
+  -- Mason can't be VeryLazy as well due to LSP on 1st file read
+  config = function()
+    require("auto-session").setup({
+      auto_session_allowed_dirs = { "~/code/*", "~/.config/nvim" },
+      pre_save_cmds = { "Neotree close", "TroubleClose" },
+    })
+  end,
 }
