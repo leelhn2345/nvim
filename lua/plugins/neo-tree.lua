c4d8f288370aa97f15237bf2777dc7ad26e38299
Nelson Lee <leelhn2345@outlook.com>
2024-01-14 01:10:24 +0800

    chore: config neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,41 +1,42 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			default_component_configs = {
-				indent = {
-					with_expanders = true,
-				},
-				git_status = {
-					symbols = {
-						ignored = "",
-					},
-				},
-			},
-			filesystem = {
-				follow_current_file = {
-					enabled = true,
-				},
-				filtered_items = {
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					-- don't risk messing with git config
-					never_show = { ".git" },
-				},
-			},
-		})
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      default_component_configs = {
+        indent = {
+          with_expanders = true,
+        },
+        git_status = {
+          symbols = {
+            ignored = "",
+          },
+        },
+      },
+      filesystem = {
+        hijack_netrw_behavior = "open_current",
+        follow_current_file = {
+          enabled = true,
+        },
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          -- don't risk messing with git config
+          never_show = { ".git" },
+        },
+      },
+    })
 
-		opts.desc = "Focus File Explorer"
-		keymap.set("n", "<leader>em", ":Neotree focus<CR>", opts)
+    opts.desc = "Focus File Explorer"
+    keymap.set("n", "<leader>em", ":Neotree focus<CR>", opts)
 
-		opts.desc = "Toggle File Explorer"
-		keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", opts)
-	end,
+    opts.desc = "Toggle File Explorer"
+    keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", opts)
+  end,
 }
