1a76ee7993c707728ab0e16d1abd4672f5f661ce
Nelson Lee <leelhn2345@outlook.com>
2024-02-07 04:06:21 +0800

    feat!: gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -34,6 +34,25 @@ return {
       })
     end,
   },
+
   -- Git Blame
-  { "f-person/git-blame.nvim", event = "VeryLazy" },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
+    config = function()
+      -- local blame_format = "  <author> • <author_time:%d %b %Y> • <summary>"
+      local blame_format =
+        "  <author> • <author_time:%a %d %b %Y %H:%M:%S> <author_tz> • <summary>"
+
+      require("gitsigns").setup({
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 0,
+          ignore_whitespace = true,
+          virt_text_priority = 5000,
+        },
+        current_line_blame_formatter = blame_format,
+      })
+    end,
+  },
 }
