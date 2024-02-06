ba490d2783c741f82bdb00e5c9d475d72209f42f
Nelson Lee <leelhn2345@outlook.com>
2024-02-06 15:23:09 +0800

    feat: nice keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,22 +1,42 @@
-local keymap = vim.keymap
-
 local opts = { noremap = true, silent = true }
 
 -- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select All" })
+vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select All" })
 
 -- Window Management
-keymap.set("n", "<leader>ss", ":vsplit<CR>", opts) -- Split Vertical
-keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontal
-
--- Comments
-local comment_opts = { noremap = false, desc = "Toggle Comment" }
-vim.api.nvim_set_keymap("n", "<C-_>", "gcc", comment_opts)
-vim.api.nvim_set_keymap("v", "<C-_>", "gcc", comment_opts)
+vim.keymap.set("n", "<leader>ss", ":vsplit<CR>", opts) -- Split Vertical
+vim.keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontal
 
 -- Indenting
-keymap.set("v", "<", "<gv", { desc = "Outdent" })
-keymap.set("v", ">", ">gv", { desc = "Indent" })
+vim.keymap.set("v", "<", "<gv", { desc = "Outdent" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent" })
 
 -- Unmap keys
-keymap.set("n", "q", "<Nop>", opts) -- I don't use macros at all... yet.
+vim.keymap.set("n", "q", "<Nop>") -- I don't use macros at all... yet.
+
+-- Move selected lines with shift+j or shift+k
+vim.keymap.set(
+  "v",
+  "J",
+  ":m '>+1<CR>gv=gv",
+  { desc = "move selected lines down" }
+)
+vim.keymap.set(
+  "v",
+  "K",
+  ":m '<-2<CR>gv=gv",
+  { desc = "move selected lines up" }
+)
+
+-- Copy to system clipboard
+vim.keymap.set(
+  { "n", "v" },
+  "<leader>y",
+  [["+y]],
+  { desc = "Copy to system clipboard" }
+)
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy to system clipboard" })
+
+-- Keep cursor centred while scrolling up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up" })
--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,4 +1,11 @@
 return {
-	"numToStr/Comment.nvim",
-	opts = {},
+  "numToStr/Comment.nvim",
+  config = function()
+    require("Comment").setup()
+
+    local opts = { noremap = false, desc = "Toggle Comment" }
+
+    vim.api.nvim_set_keymap("n", "<C-_>", "gcc", opts)
+    vim.api.nvim_set_keymap("v", "<C-_>", "gcc", opts)
+  end,
 }
