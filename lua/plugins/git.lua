36d9cf587b2ecc25e149019cbab524801293d040
Nelson Lee <leelhn2345@outlook.com>
2024-02-07 04:22:05 +0800

    chore: change blame format

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -40,9 +40,9 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
     config = function()
-      -- local blame_format = "  <author> • <author_time:%d %b %Y> • <summary>"
-      local blame_format =
-        "  <author> • <author_time:%a %d %b %Y %H:%M:%S> <author_tz> • <summary>"
+      local blame_format = "  <author> • <author_time:%d %b %Y> • <summary>"
+      -- local blame_format =
+      --   "  <author> • <author_time:%a %d %b %Y %H:%M:%S> <author_tz> • <summary>"
 
       require("gitsigns").setup({
         current_line_blame = true,
