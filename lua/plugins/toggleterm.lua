0faec1ff912015df2fab5a70d8844c14a94b49b6
Nelson Lee <leelhn2345@outlook.com>
2024-02-09 22:23:11 +0800

    fix: disable keymap in insert mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,12 +3,12 @@ return {
   version = "*",
   cmd = "ToggleTerm",
   keys = {
-    { "\\", ":ToggleTerm", desc = "ToggleTerm" },
+    { "\\", ":ToggleTerm", desc = "Toggle Terminal" },
     { "<esc>", "<C-\\><C-n>", mode = "t" },
   },
   opts = {
     open_mapping = [[\]],
     direction = "float",
+    insert_mappings = false,
   },
-  config = true,
 }
