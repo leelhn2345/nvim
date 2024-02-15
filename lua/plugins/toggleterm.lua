bb78d003d7ee61808f5a6ce3d1fc0dc91aa882f9
Nelson Lee <leelhn2345@outlook.com>
2024-02-16 01:37:19 +0800

    feat: vertical toggle term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,10 +5,18 @@ return {
   keys = {
     { "\\", ":ToggleTerm", desc = "Toggle Terminal" },
     { "<esc>", "<C-\\><C-n>", mode = "t" },
+    { "<leader>\\", ":TermSelect<CR>", mode = "n", desc = "Select Terminal" },
   },
   opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return vim.o.lines * 0.3
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
     open_mapping = [[\]],
-    direction = "float",
+    direction = "vertical",
     insert_mappings = false,
   },
 }
